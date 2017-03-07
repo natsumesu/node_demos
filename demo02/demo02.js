ba02aa7fa8f7bfcd64d28dd5aa81d6d664b5b30d
su <natsumesu@foxmail.com>
2017-03-07 15:40:05 +0800

    su@2017-3-7

--- demo02/demo02.js
+++ demo02/demo02.js
@@ -1,2 +1,19 @@
-require("babel-core/register");
-require("./source.js")
+function getTime(){
+	var promise = new Promise((resolve,reject) => {
+		var now = Date.now();
+		setTimeout(() => {
+			if(now%2==0){
+				resolve(now);
+			}else{
+				reject(now);
+			}
+		},500)
+	});
+	return promise;
+}
+
+getTime().then(n => {
+	console.log(`OK:${n}`);
+}).catch( n => {
+	console.log(`Error:${n}`)
+});
